bfbaaec3936a45f03da7823d71bc0f81a9b51452
KarinaPelykh <127746589+KarinaPelykh@users.noreply.github.com>
2023-07-28 18:31:58 +0200

    ADED:changes-markup and css

--- src/components/CardUser/CardUser.jsx
+++ src/components/CardUser/CardUser.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { featchUsers } from "../../redux/operation";
-import { Button } from "../Button/Button";
+import picture from "../../images/picture.png";
+import icon from "../../images/sprite.svg";
 import { selectUser } from "../../redux/selector";
 import css from "./CardUser.module.css";
 export const CardUser = () => {
@@ -11,15 +12,25 @@ export const CardUser = () => {
     dispatch(featchUsers());
   }, [dispatch]);
   return (
-    <>
+    <ul>
       {users.map((user) => (
-        <div key={user.id} className={css.wrap}>
-          <img src={user.avatar} alt="Avatar" />
-          <p>{user.tweets} TWEETS</p>
-          <p>{user.followers} FOLLOWERS</p>
-          <Button />
-        </div>
+        <li key={user.id} className={css.wrap}>
+          <div>
+            <svg className={css.Svg} style={{ width: "76px", height: "22px" }}>
+              <use xlinkHref={icon + "#Logo"}></use>
+            </svg>
+            <img src={picture} />
+          </div>
+          <div className={css.line}>
+            <img className={css.images} src={user.avatar} alt="Avatar" />
+            <p>{user.tweets} TWEETS</p>
+            <p>{user.followers} FOLLOWERS</p>
+            <button className={css.button} type="button">
+              FOLLOW
+            </button>
+          </div>
+        </li>
       ))}
-    </>
+    </ul>
   );
 };
